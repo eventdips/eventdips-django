70ae89686aae01e74cddf0becc4ea480aeab7701
Aayush Dani <aayushdani1@gmail.com>
2019-10-19 12:17:21 +0530

    changes

--- teacherview/views.py
+++ teacherview/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Events,SubEvents,Status
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 from studentview.models import Registrations
 from django.http import HttpResponseRedirect
 from django.contrib import messages
@@ -78,6 +79,8 @@ SORT EVENTS BY REGISTRATION DEADLINES- STUDENTVIEW
 
 @login_required
 def home(request):
+    login(request,request.user)
+    
     if student_check(request):
         return redirect('student-homepage')
 
