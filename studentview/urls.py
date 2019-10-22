b4ab6e4a6789ce7d22ffb5d85d7aba41e392afc8
Aayush Dani <aayushdani1@gmail.com>
2019-10-22 15:12:01 +0530

    student

--- studentview/urls.py
+++ studentview/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('',views.home,name="student-homepage"),
-	path('profile',views.profile,name="student-profile")
+	path('profile',views.profile,name="student-profile"),
+	path('acheivements',views.my_acheivements,name="student-acheivements")
 ]
--- studentview/views.py
+++ studentview/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from teacherview.models import Status
 
 def home(request):
 	return render(request,'studentview/home.html')
 
 def profile(request):
 	return render(request,'studentview/myProfile.html')
+
+def my_acheivements(request):
+	id_ = int(request.COOKIES.get('id')) 
+	user = User.objects.get(pk=id_)
+	status = Status.objects.get(user=user)
+	#status.status == "S", status.acheivements = "(acheivement_name,acheivement_category,acheivement_description,acheivement_date);(achei....)"
+
+	context ={
+		
+	}
+	return render(request,'studentview/my_acheivements.html',context)
