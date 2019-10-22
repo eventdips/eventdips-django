b06a6d90a849cbe297f41da9d4934b8705e30ef8
arham musheer <arhammusheer@gmail.com>
2019-10-23 00:48:41 +0530

    removed borken

--- EVENTdips/settings.py
+++ EVENTdips/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -160,5 +159,3 @@ EMAIL_HOST_USER = 'support@eventdips.ga'
 EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_TLS = True
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
