83716efb03404578c1298eba44db0696339348ec
arham musheer <arhammusheer@gmail.com>
2020-01-09 18:50:56 +0530

    email test

--- EVENTdips/settings.py
+++ EVENTdips/settings.py
@@ -157,10 +157,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'eventdips@gmail.com'
 EMAIL_HOST_PASSWORD = 'eventdips123'
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 
 
 USER_AGENTS_CACHE = 'default'
